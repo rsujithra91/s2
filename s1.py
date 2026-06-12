bff8b10fce4013f1dfa73c895fa54823c4d05768
rsujithra91 <rsujithra91@gmail.com>
2026-06-12 11:00:13 +0530

    Update s1.py

--- s1.py
+++ s1.py
@@ -1,4 +1,7 @@
 import streamlit as st
 st.title("My first streamlit application")
 name=st.text_input("Enter your name")
+n1=st.number_input("Enter your age")
+if st.button("Submit"):
+         st.write(f"Your name is {name},age is  {n1}")
          
